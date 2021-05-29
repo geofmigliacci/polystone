35a9c8fcef7a5cf426a6cd72b711efcd8a86d0e7
Geoffrey <geoffrey.migliacci@ellyos.fr>
2021-05-29 18:02:57 +0200

    chre(readme): push read me

--- Modules/Polystone.Modules.Catch/ViewModels/CatchViewModel.cs
+++ Modules/Polystone.Modules.Catch/ViewModels/CatchViewModel.cs
@@ -1,16 +1,65 @@
-﻿using Prism.Commands;
+﻿using Microsoft.EntityFrameworkCore;
+using POGOProtos.Rpc;
+using Polystone.Business.Models;
+using Polystone.Services.Interfaces;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Polystone.Modules.Catch.ViewModels
 {
+    public class StackingBarChartModel : BindableBase
+    {
+        public string Specie { get; set; }
+        public int Candy { get; set; }
+        public int XLCandy { get; set; }
+    }
+    public class DataTableCatch : BindableBase
+    {
+        public string Specie { get; set; }
+        public DateTime CatchDate { get; set; }
+        public int Cp { get; set; }
+        public int Experience { get; set; }
+        public int Stardust { get; set; }
+        public bool IsShiny { get; set; }
+        public bool IsShadow { get; set; }
+    }
+
     public class CatchViewModel : BindableBase
     {
-        public CatchViewModel()
-        {
+        private IPolystoneAccountService _polystoneAccountService;
+        private IPolystoneContextService _polystoneContextService;
 
+        public ObservableCollection<DataTableCatch> DataTableCatches { get; set; }
+
+        public CatchViewModel(
+            IPolystoneContextService polystoneContextService,
+            IPolystoneAccountService polystoneAccountService
+        )
+        {
+            _polystoneAccountService = polystoneAccountService;
+            _polystoneContextService = polystoneContextService;
+
+            Account currentAccount = _polystoneAccountService.GetAccount();
+            IEnumerable<AccountCatch> accountCatches = _polystoneContextService.GetPolystoneContext().Accounts.Include(a_ => a_.AccountCatches).FirstOrDefault(
+                a_ => a_.Name == currentAccount.Name
+            ).AccountCatches.Where(c_ => 
+                c_.Specie > -1
+            );
+
+            DataTableCatches = new ObservableCollection<DataTableCatch>(accountCatches.Select(c_ => new DataTableCatch()
+            {
+                Specie = ((HoloPokemonId)c_.Specie).ToString("g"),
+                CatchDate = c_.CatchDate.Value,
+                Cp = c_.Cp,
+                Experience = c_.Experience,
+                Stardust = c_.Stardust,
+                IsShiny = c_.IsShiny,
+                IsShadow = c_.IsShadow,
+            }));
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+[![License](https://img.shields.io/badge/General%20Public%20License%203.0-lightgrey.svg?style=for-the-badge)](LICENSE.md)
+
+[![SonarCloud](https://img.shields.io/sonar/quality_gate/polystone?server=https%3A%2F%2Fsonarcloud.io&style=for-the-badge)](https://sonarcloud.io/dashboard?id=polystone)
+
+[![Twitter Followers](https://img.shields.io/twitter/follow/myerffoeg.svg?logo=twitter&style=for-the-badge&label=Follow)](https://twitter.com/myerffoeg)
+[![Github Followers](https://img.shields.io/github/followers/myerffoeg?logo=github&style=for-the-badge)](https://github.com/myerffoeg)
+[![PayPal](https://img.shields.io/badge/Donate-PayPal-ff3f59.svg?style=for-the-badge)](https://www.paypal.me/myerffoeg)
+
+# Polystone
+
+Polystone is a stat gathering tool that helps you gather and display useful human readable data from the Polygon# application.
